4bba51a70d6869555b7af6e41e49bd82b318f5f4
JaeCgngn <jaecgngn@gmail.com>
2026-01-07 02:20:39 +0800

    Final

--- Assets/Scripts/NoGoZone.cs
+++ Assets/Scripts/NoGoZone.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NoGoZone : MonoBehaviour
 {
     public Transform player;
     public float shakeProximity = 0.5f;
     public float shakeStrength = 0.1f;
-    public float shakeDistance = 5f;
-    public float shake = 2f;
+    public float failDistance = 1f;
 
     private Vector3 originalPosition;
 
@@ -17,40 +17,29 @@ public class NoGoZone : MonoBehaviour
         GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
         player = playerObject.transform;
 
-        originalPosition = transform.position;
+        originalPosition = transform.localPosition;
     }
 
     void Update()
     {
-         float distance = player.position.magnitude;
-        if (distance < shakeProximity) 
-        { 
-            float proximityStrength = Mathf.Lerp(0f, shakeStrength, 1f - (distance / shakeProximity));
+        float distance = Vector3.Distance(player.position, transform.position);
 
-            StartCoroutine(Shake());
-
-        }
-
-
-    }
-
-    private IEnumerator Shake()
-    {
-        float elapsed = 0f;
-
-        while (elapsed < shakeDistance)
+        if (distance < shakeProximity)
         {
-            float x = Random.Range (-1f, 1f) * shakeStrength;
+
+            float x = Random.Range(-1f, 1f) * shakeStrength;
             float y = Random.Range(-1f, 1f) * shakeStrength;
 
             transform.localPosition = originalPosition + new Vector3 (x, y, 0f);
 
-            elapsed += Time.deltaTime;
-
-            yield return null;
+            if (distance < failDistance)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
         }
-
-        transform.localPosition = originalPosition;
-
+        else
+        {
+            transform.position = originalPosition;
+        }
     }
 }
--- Assets/Scripts/WinZone.cs
+++ Assets/Scripts/WinZone.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class WinZone : MonoBehaviour
 {
+
+    public Transform player;
+    public float winDistance = 1f;
+
+    public GameObject winUI;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +17,12 @@ public class WinZone : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float distance = Vector3.Distance(player.position, transform.position);
+
+        if (distance < winDistance)
+        {
+            winUI.SetActive(true);
+            Time.timeScale = 0f; 
+        }
     }
 }
